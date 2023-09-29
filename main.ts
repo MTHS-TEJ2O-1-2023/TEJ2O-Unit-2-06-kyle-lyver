55471bb8a932e042b3db259d71c654abe6aa7b8f
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-09-29 13:05:32 -0400

    fixed indentation

--- main.ts
+++ main.ts
@@ -14,12 +14,12 @@ basic.showIcon(IconNames.Happy)
 
 // turn on LED
 input.onButtonPressed(Button.A, function () {
-basic.showIcon(IconNames.Yes)
-pins.digitalWritePin(DigitalPin.P16, 1)
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
 // turn off LED
 input.onButtonPressed(Button.B, function () {
-basic.showIcon(IconNames.No)
-pins.digitalWritePin(DigitalPin.P16, 0)
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
 })
